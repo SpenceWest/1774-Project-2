2f480ced70ac52f5fabc6c410222fb6db2fec7a8
GongduoHuang <1287149627@qq.com>
2026-02-17 08:58:40 -0500

    Add type hints to Load class constructor

--- Load.py
+++ Load.py
@@ -1,5 +1,5 @@
 class Load:
-    def __init__(self, name, bus1_name, mw, mvar):
+    def __init__(self, name: str, bus1_name: str, mw: float, mvar: float):
         self.name = name
         self.bus1_name = bus1_name
         self.mw = mw
