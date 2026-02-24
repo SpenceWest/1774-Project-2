cb7d7106ed25861ff3b59b623fd1719c2c06675c
SpenceWest <slw138@pitt.edu>
2026-02-24 08:50:49 -0500

    milestone 4 test

--- circuit.py
+++ circuit.py
@@ -12,8 +12,7 @@ class Circuit:
         self.transmission_lines = {}
         self.loads = {}
         self.generators = {}
-        self.ybus
-
+        self.ybusss
 
     def add_bus(self, bus_name:str, nominal_kv:float):
 
